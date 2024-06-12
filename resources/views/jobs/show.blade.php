4a2d36bc48bbdd64b1e600122b81d9c2ddaac736
Goodness Obi <goodnessobi@gmail.com>
2024-06-12 12:42:27 +0100

    folder structure reorganization

--- resources/views/jobs/show.blade.php
+++ resources/views/jobs/show.blade.php
@@ -4,8 +4,6 @@
     </x-slot:heading>
 
     <h2 class="font-bold text-lg"> {{ $job['title'] }} </h2>
+
     <p>This job pays {{ $job['salary']}} </p>
 </x-layout>
-
-
-
--- routes/web.php
+++ routes/web.php
@@ -6,21 +6,24 @@ use App\Models\Job;
 
 Route::get('/', function () {
 
-    return view('home');
+    return view('home.index');
 });
 
 Route::get('/jobs', function ()  {
     $jobs = Job::with('employer')->simplePaginate(3);
 
-    return view('jobs', ['jobs' => $jobs]);
+    return view('jobs.index', ['jobs' => $jobs]);
+});
+
+Route::get('/jobs/create', function () {
+    return view('jobs.create');
 });
 
 Route::get('/jobs/{id}', function ($id) {
     $job = Job::find($id);
-    return view('job', ['job' => $job]);
+    return view('jobs.show', ['job' => $job]);
 });
 
-
 Route::get('/contact', function () {
-    return view('contact');
+    return view('contact.index');
 });
